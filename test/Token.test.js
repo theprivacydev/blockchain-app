bd5d05d1b36f54f7aa4e78bab24bafa983f47672
The Privacy Dev <theprivacydev@protonmail.com>
2022-05-28 16:15:08 -0500

    redefined token name and defined symbol, decimals, and total supply in Token.sol smart contract and wrote tests for the contract to ensure all currently pass and function as intended

--- test/Token.test.js
+++ test/Token.test.js
@@ -2,14 +2,35 @@ const Token = artifacts.require("./Token");
 require("chai").use(require("chai-as-promised")).should();
 
 contract("Token", (accounts) => {
+  const name = "Fortune Token";
+  const symbol = "FTN";
+  const decimals = "18";
+  const totalSupply = "1000000000000000000000000";
+  let token;
+
+  beforeEach(async () => {
+    token = await Token.new();
+  });
+
   describe("deployment", () => {
     it("tracks the name", async () => {
-      // Fetch token from blockchain
-      const token = await Token.new();
-      //Read the token name here...
       const result = await token.name();
-      // Check the token name is 'My Name'
-      result.should.equal("My Name");
+      result.should.equal(name);
+    });
+
+    it("tracks the symbol", async () => {
+      const result = await token.symbol();
+      result.should.equal(symbol);
+    });
+
+    it("tracks the decimals", async () => {
+      const result = await token.decimals();
+      result.toString().should.equal(decimals);
+    });
+
+    it("tracks the total supply", async () => {
+      const result = await token.totalSupply();
+      result.toString().should.equal(totalSupply);
     });
   });
 });
