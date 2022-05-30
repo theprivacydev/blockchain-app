ba454a021de6909af3c0cc806ddbc160eba2e91d
The Privacy Dev <theprivacydev@protonmail.com>
2022-05-30 18:41:09 -0500

    added require statements in transfer function to ensure that _to address is a valid recipient and balances are sufficient for transfer and added tests to confirm functionality that are successful

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -25,6 +25,8 @@ contract Token {
         public
         returns (bool success)
     {
+        require(_to != address(0));
+        require(balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
         balanceOf[_to] = balanceOf[_to].add(_value);
         emit Transfer(msg.sender, _to, _value);
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,4 +1,4 @@
-import { tokens } from "./helpers";
+import { tokens, EVM_revert } from "./helpers";
 
 const Token = artifacts.require("./Token");
 
@@ -46,27 +46,51 @@ contract("Token", ([deployer, receiver]) => {
     let result;
     let amount;
 
-    beforeEach(async () => {
-      amount = tokens(100);
-      result = await token.transfer(receiver, amount, { from: deployer });
-    });
-    it("transfers token balances", async () => {
-      let balanceOf;
-      balanceOf = await token.balanceOf(deployer);
-      balanceOf.toString().should.equal(tokens(999900).toString());
-      balanceOf = await token.balanceOf(receiver);
-      balanceOf.toString().should.equal(tokens(100).toString());
+    describe("success", async () => {
+      beforeEach(async () => {
+        amount = tokens(100);
+        result = await token.transfer(receiver, amount, { from: deployer });
+      });
+      it("transfers token balances", async () => {
+        let balanceOf;
+        balanceOf = await token.balanceOf(deployer);
+        balanceOf.toString().should.equal(tokens(999900).toString());
+        balanceOf = await token.balanceOf(receiver);
+        balanceOf.toString().should.equal(tokens(100).toString());
+      });
+
+      it("emits a transfer event", async () => {
+        const log = result.logs[0];
+        log.event.should.eq("Transfer");
+        const event = log.args;
+        event.from.toString().should.equal(deployer, "from is correct");
+        event.to.toString().should.equal(receiver, "to is correct");
+        event.value
+          .toString()
+          .should.equal(amount.toString(), "value is correct");
+      });
     });
 
-    it("emits a transfer event", async () => {
-      const log = result.logs[0];
-      log.event.should.eq("Transfer");
-      const event = log.args;
-      event.from.toString().should.equal(deployer, "from is correct");
-      event.to.toString().should.equal(receiver, "to is correct");
-      event.value
-        .toString()
-        .should.equal(amount.toString(), "value is correct");
+    describe("failure", async () => {
+      it("rejects insufficient balances", async () => {
+        let invalidAmount;
+        invalidAmount = tokens(100000000); // 100 million - greater than total supply
+        await token
+          .transfer(receiver, invalidAmount, { from: deployer })
+          .should.be.rejectedWith(EVM_revert);
+
+        // Attempt transfer tokens when you have none
+        invalidAmount = tokens(10); //receipient has no tokens
+        await token
+          .transfer(deployer, invalidAmount, { from: receiver })
+          .should.be.rejectedWith(EVM_revert);
+      });
+
+      it("rejects invalid recipients", async () => {
+        await token
+          .transfer(0x0, amount, { from: deployer })
+          .should.be.rejected;
+      });
     });
   });
 });
