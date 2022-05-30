f5a1f20163041744b08a6d231c60cec9298ea257
The Privacy Dev <theprivacydev@protonmail.com>
2022-05-30 17:38:50 -0500

    created transfer function in Token.sol to transfer tokens and created helpers.js file with tokens function that converts wei to ether with web3.js

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,11 +1,14 @@
+import { tokens } from "./helpers";
+
 const Token = artifacts.require("./Token");
+
 require("chai").use(require("chai-as-promised")).should();
 
-contract("Token", (accounts) => {
+contract("Token", ([deployer, receiver]) => {
   const name = "Fortune Token";
   const symbol = "FTN";
   const decimals = "18";
-  const totalSupply = "1000000000000000000000000";
+  const totalSupply = tokens(1000000).toString();
   let token;
 
   beforeEach(async () => {
@@ -30,7 +33,34 @@ contract("Token", (accounts) => {
 
     it("tracks the total supply", async () => {
       const result = await token.totalSupply();
-      result.toString().should.equal(totalSupply);
+      result.toString().should.equal(totalSupply.toString());
+    });
+
+    it("assigns the total supply to the deployer", async () => {
+      const result = await token.balanceOf(deployer);
+      result.toString().should.equal(totalSupply.toString());
+    });
+  });
+
+  describe("sending tokens", () => {
+    it("transfers token balances", async () => {
+      let balanceOf;
+      //Before transfer
+      balanceOf = await token.balanceOf(deployer);
+      console.log("deployer balance before transfer", balanceOf.toString());
+      balanceOf = await token.balanceOf(receiver);
+      console.log("receiver balance before transfer", balanceOf.toString());
+
+      //Transfer
+      await token.transfer(receiver, tokens(100), {
+        from: deployer
+      });
+
+      //After transfer
+      balanceOf = await token.balanceOf(deployer);
+      console.log("deployer balance after transfer", balanceOf.toString());
+      balanceOf = await token.balanceOf(receiver);
+      console.log("receiver balance after transfer", balanceOf.toString());
     });
   });
 });
