3363b4770d224860ae284f67093a3885fa05caf4
The Privacy Dev <theprivacydev@protonmail.com>
2022-06-08 17:42:08 -0500

    added event in exchange for depositing tokens and added test for event emission which passes successfully

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -5,11 +5,14 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Exchange {
     using SafeMath for uint256;
-    
-    //Variables
+
+    // Variables
     address public feeAccount; 
     uint256 public feePercent; 
     mapping(address => mapping(address => uint256)) public tokens;
+
+    // Events
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
     
     constructor (address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
@@ -19,6 +22,7 @@ contract Exchange {
     function depositToken(address _token, uint256 _amount) public {
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
         tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
         // Send token to contract
         // Manage deposit - update balance
         // emit event
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -40,7 +40,7 @@ contract("Exchange", ([deployer, feeAccount, user1]) => {
     beforeEach(async () => {
       amount = tokens(10);
       await token.approve(exchange.address, amount, { from: user1 });
-      const result = await exchange.depositToken(token.address, amount, {
+      result = await exchange.depositToken(token.address, amount, {
         from: user1
       });
     });
@@ -54,6 +54,20 @@ contract("Exchange", ([deployer, feeAccount, user1]) => {
         //Check tokens on exchange
         balance = await exchange.tokens(token.address, user1);
         balance.toString().should.equal(amount.toString());
+      });
+
+      it("emits a Deposit event", async () => {
+        const log = result.logs[0];
+        log.event.should.eq("Deposit");
+        const event = log.args;
+        event.token.should.equal(token.address, "token address correct");
+        event.user.should.equal(user1, "user address is correct");
+        event.amount
+          .toString()
+          .should.equal(tokens(10).toString(), "amount is correct");
+        event.balance
+          .toString()
+          .should.equal(tokens(10).toString(), "balance is correct");
       });
     });
 
