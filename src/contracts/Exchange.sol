22eb0091a0f7ae2d13fd4e8a22cf1258a508cddc
The Privacy Dev <theprivacydev@protonmail.com>
2022-06-07 21:20:09 -0500

    created logic in smart contract and matching test to track fee percent and it passes

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.5.0;
 
 contract Exchange {
     //Variables
-    address public feeAccount; // the account that receives exchange fees
+    address public feeAccount; 
+    uint256 public feePercent; 
     
-    constructor (address _feeAccount) public {
+    constructor (address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
+        feePercent = _feePercent;
     }
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -7,15 +7,21 @@ require("chai").use(require("chai-as-promised")).should();
 
 contract("Exchange", ([deployer, feeAccount]) => {
   let exchange;
+  const feePercent = 10;
 
   beforeEach(async () => {
-    exchange = await Exchange.new(feeAccount);
+    exchange = await Exchange.new(feeAccount, feePercent);
   });
 
   describe("deployment", () => {
     it("tracks the fee account", async () => {
       const result = await exchange.feeAccount();
       result.should.equal(feeAccount);
+    });
+
+    it("tracks the fee percent", async () => {
+      const result = await exchange.feePercent();
+      result.toString().should.equal(feePercent.toString());
     });
   });
 });
