9efdb573df78e53f2f78d49f70b1a49837ecaff6
The Privacy Dev <theprivacydev@protonmail.com>
2022-06-18 15:42:22 -0500

    created fillOrder and _trade funtions in Exchange.sol

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -41,6 +41,16 @@ contract Exchange {
         uint256 amountGive,
         uint256 timestamp
     );
+    event Trade(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,,
+        address userFill,
+        uint256 timestamp
+    );
 
     // Structs
     struct _Order {
@@ -108,5 +118,27 @@ contract Exchange {
         require(_order.id == _id);
         orderCancelled[_id] = true;
         emit Cancel(orderCount, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, now);
+    }
+
+    function fillOrder(uint256 _id) public {
+        _Order storage _order = orders[_id];
+        _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+    }
+
+    function _trade(uint256 _orderId, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal {
+        // Fee paid by the user that fills the order, aka msg.sender
+        // Fee deducted from _amountGet
+        uint256 _feeAmount = _amountGive.mul(feePercent).div(100);
+
+
+        // Execute trade
+        tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount));
+        tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
+        tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount].add(_feeAmount);
+        tokens[_tokenGive][_user] = tokens[_tokenGive][_user].sub(_amountGive);
+        tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGet);
+
+        // Emit Trade Event
+        emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, now);
     }
 }
