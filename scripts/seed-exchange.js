d491157c3b46416aadce21d2a06881adca71cff7
The Privacy Dev <theprivacydev@protonmail.com>
2022-06-23 19:57:04 -0500

    successfully imported and retreieved deployed token and exchange in seed-exchange.js file

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -1,6 +1,19 @@
+const Token = artifacts.require("Token");
+const Exchange = artifacts.require("Exchange");
+
 module.exports = async function (callback) {
   try {
-    console.log("script runnings");
+    //Fetch accounts 
+    const accounts = await web3.eth.getAccounts();
+
+    //Fetch deployed token
+    const token = await Token.deployed();
+    console.log("token fetched: ", token.address);
+
+    //Fetch the deployed exchange
+    const exchange = await Exchange.deployed();
+    console.log("exchange fetched: ", exchange.address);
+
   } catch (err) {
     console.log(err);
   }
