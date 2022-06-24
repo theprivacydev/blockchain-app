b118ace703c8fc4012c7ea89fde931cf574386b4
The Privacy Dev <theprivacydev@protonmail.com>
2022-06-23 19:40:31 -0500

    added Exchange to deployer migrations file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const Token = artifacts.require("Token");
+const Exchange = artifacts.require("Exchange");
 
-module.exports = function (deployer) {
-  deployer.deploy(Token);
+module.exports = async function (deployer) {
+  await deployer.deploy(Token);
+  await deployer.deploy(Exchange, "fee account", 10);
 };
