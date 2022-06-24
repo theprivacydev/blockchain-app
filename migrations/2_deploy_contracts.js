4b4ed9fd6cbfc72ce71b4c16649df534ab9bd287
The Privacy Dev <theprivacydev@protonmail.com>
2022-06-23 19:43:48 -0500

    refactored deploy_contracts file to be functional and readable

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,12 @@ const Token = artifacts.require("Token");
 const Exchange = artifacts.require("Exchange");
 
 module.exports = async function (deployer) {
+  const accounts = await web3.eth.getAccounts();
+
   await deployer.deploy(Token);
-  await deployer.deploy(Exchange, "fee account", 10);
+
+  const feeAccount = accounts[0];
+  const feePercent = 10;
+
+  await deployer.deploy(Exchange, feeAccount, feePercent);
 };
