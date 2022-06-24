9b81493e600c1a422ec2cb393b89faa57aad113f
The Privacy Dev <theprivacydev@protonmail.com>
2022-06-23 20:06:41 -0500

    added appropriate imports for helper functions and set up users for exchange and had them deposit tokens inside seed-exchange script

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -1,3 +1,5 @@
+const { ether, tokens } = require("../test/helpers");
+
 const Token = artifacts.require("Token");
 const Exchange = artifacts.require("Exchange");
 
@@ -21,6 +23,26 @@ module.exports = async function (callback) {
 
     await token.transfer(receiver, amount, { from: sender });
     console.log(`transferred ${amount} tokens from ${sender} to ${receiver}`);
+
+    // Set up exchange users
+    const user1 = accounts[0];
+    const user2 = accounts[1];
+
+    // User 1 Deposits Ether
+    amount = 1;
+    await exchange.depositEther({ from: user1, value: ether(amount) });
+    console.log(`Deposited: ${amount} Ether from ${user1}`);
+
+    // User 2 approves tokens
+    amount = 10000;
+    await token.approve(exchange.address, tokens(amount), { from: user2 });
+    console.log(`Approve ${amount} tokens from ${user2}`);
+
+    // User 2 Deposits tokens
+    await exhcnage.depositToken(token.address, tokens(amount), { from: user2 });
+    console.log(`Deposited ${amount} tokens from ${user2}`);
+
+
   } catch (err) {
     console.log(err);
   }
