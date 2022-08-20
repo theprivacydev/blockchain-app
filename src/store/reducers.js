cfe534693ef46aac0d58ae18fea4b0fa8799c8f0
The Privacy Dev <theprivacydev@protonmail.com>
2022-08-20 18:54:05 -0500

    imported combine reducer and exported rootReducer with redux in reducers.js file

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,6 +1,14 @@
-function web3(state = { }, action) {
-    switch (action.type) {
-      default:
-        return state
-    }
+import { combineReducers } from "redux";
+
+function web3(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
+
+const rootReducer = combineReducers({
+  web3: web3
+});
+
+export default rootReducer;
