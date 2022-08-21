c69951946c6ea2f01a55be33b6a0f074049fef59
The Privacy Dev <theprivacydev@protonmail.com>
2022-08-20 19:30:26 -0500

    imported redux and configureStore in index.js and wrapped App in Provider, confirmed redux was initialed with redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import configureStore from "./store/configureStore";
+import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
