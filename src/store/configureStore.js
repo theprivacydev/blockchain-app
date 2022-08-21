ea12f65d41b09b276cd12545cfeee2a425aff54a
The Privacy Dev <theprivacydev@protonmail.com>
2022-08-20 19:24:06 -0500

    redux store is successfully configured now with middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,17 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import logger, { createLogger } from "redux-logger";
 import rootReducer from "./reducers";
 
+const loggerMiddleware = createLogger();
+const middleware = [];
+
+//For Redux Dev Tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default function configureStore(preloadedState) {
-  return createStore(rootReducer);
+  return createStore(
+    rootReducer,
+    preloadedState,
+    composeEnhancers(applyMiddleware(...middleware, loggerMiddleware))
+  );
 }
