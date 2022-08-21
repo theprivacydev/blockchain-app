1f735d4d3098c7399bd47ca860e84afffccf7ff6
The Privacy Dev <theprivacydev@protonmail.com>
2022-08-20 19:25:29 -0500

    removed logger import as it was not necessary and unused

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import logger, { createLogger } from "redux-logger";
+import { createLogger } from "redux-logger";
 import rootReducer from "./reducers";
 
 const loggerMiddleware = createLogger();
